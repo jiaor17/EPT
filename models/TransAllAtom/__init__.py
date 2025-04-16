ec89e47d985c6dac57d5bd0e07224ca370a063d9
jiaor17 <jiaor21@mails.tsinghua.edu.cn>
2025-04-16 14:15:59 +0800

    fix import

--- models/TransAllAtom/__init__.py
+++ models/TransAllAtom/__init__.py
@@ -1,6 +1,1 @@
-# from .trans_all import TransEncoder
-from .xtrans_all import XTransEncoder
-from .xtrans_dev import XTransEncoderDev
 from .xtrans_act import XTransEncoderAct
-# from .flash_trans_all import FlashTransEncoder
-# from .flash_trans_all_bias import FlashTransBiasEncoder
